6928b27ca20dc30c8b2762855e47a95545fdaf29
ShantiMishra <shantimishra2006@gmail.com>
2025-12-12 13:58:09 +0530

    List Stream

--- lecture16/streamList.java
+++ lecture16/streamList.java
@@ -6,13 +6,16 @@ import java.util.*;
 public class streamList {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(50);
-        list.add(60);
-        list.add(70);
-        list.add(80);
-        List<Integer> res = list.stream().filter(n->n>60).toList();
-        List<Integer> Mapres = list.stream().map(n->n*60).toList();
-        int data = list.stream().reduce(0,(a,b)->a+b);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        // list.add(9);
+        // list.add(10);
+        // list.add(11);
+        List<Integer> res = list.stream().filter(n->n>5).toList();        
+        List<Integer> Mapres = list.stream().filter(n->n%2!=0).toList();
+        int data = res.stream().reduce(0,(a,b)->a+b);
 
         System.out.println(res);
         System.out.println(Mapres);
